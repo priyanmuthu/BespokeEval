87a52b71442e3447ac5ed0418eef730d6094856d
Priyan Vaithilingam <priyanmuthu@gmail.com>
2019-01-09 17:51:52 -0800

    yaml example

--- src/editor.js
+++ src/editor.js
@@ -3,6 +3,7 @@
     const amdLoader = require('../node_modules/monaco-editor/min/vs/loader.js');
     const amdRequire = amdLoader.require;
     const amdDefine = amdLoader.require.define;
+    const fs = require('fs');
     function uriFromPath(_path) {
         var pathName = path.resolve(_path).replace(/\\/g, '/');
         if (pathName.length > 0 && pathName.charAt(0) !== '/') {
@@ -18,13 +19,23 @@
     amdRequire(['vs/editor/editor.main'], function() {
         var editor = monaco.editor.create(document.getElementById('editordiv'), {
             value: "{\n\t\"Hello\":\"world\"\n}",
-            language: "json",
+            language: "yaml",
 
             lineNumbers: "on",
             roundedSelection: false,
             scrollBeyondLastLine: false,
             readOnly: false,
-            theme: "vs-dark",
+            theme: "vs-dark"
+        });
+        
+        //Testing
+        var yamlpath = path.join(__dirname, 'example.yaml');
+        console.log(yamlpath);
+        fs.readFile(yamlpath, 'utf8', (err, res) => {
+        if (!err) {
+            editor.setModel(monaco.editor.createModel(res, 'yaml'));
+        }
+        console.log(err);
         });
     });
 })();
