450a462f28ddae17a747ec80319d3c6dd0122683
Priyan Vaithilingam <priyanmuthu@gmail.com>
2019-01-12 01:14:13 -0800

    some ts

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,16 @@
-//Place all the constants here
-const UIMAP = {}
+const fs = require('fs')
+const path = require('path');
+const YAMLPATH = path.join(__dirname, 'example.yaml');
+
+function getYAMLText(){
+    console.log('came from constants');
+    var yamlText = null;
+    try{
+        yamlText = fs.readFileSync(YAMLPATH).toString();
+    }
+    catch(err){
+        console.log(err);
+    }
+
+    return yamlText;
+}
--- src/editor.js
+++ src/editor.js
@@ -1,54 +1,43 @@
-(function() {
-    const path = require('path');
-    const amdLoader = require('../node_modules/monaco-editor/min/vs/loader.js');
+(() => {
+    const path = require("path");
+    const amdLoader = require("../node_modules/monaco-editor/min/vs/loader.js");
     const amdRequire = amdLoader.require;
     const amdDefine = amdLoader.require.define;
-    const fs = require('fs');
-    var editor;
-    function uriFromPath(_path) {
-        var pathName = path.resolve(_path).replace(/\\/g, '/');
-        if (pathName.length > 0 && pathName.charAt(0) !== '/') {
-            pathName = '/' + pathName;
+    console.log(__dirname);
+    const fs = require("fs");
+    let editor;
+    function uriFromPath(pathstr) {
+        let pathName = path.resolve(pathstr).replace(/\\/g, "/");
+        if (pathName.length > 0 && pathName.charAt(0) !== "/") {
+            pathName = "/" + pathName;
         }
-        return encodeURI('file://' + pathName);
+        return encodeURI("file://" + pathName);
     }
 
     amdRequire.config({
-        baseUrl: uriFromPath(path.join(__dirname, '../node_modules/monaco-editor/min'))
+        baseUrl: uriFromPath(path.join(__dirname, "../node_modules/monaco-editor/min"))
     });
     // workaround monaco-css not understanding the environment
-    self.module = undefined;
-    amdRequire(['vs/editor/editor.main'], function() {
-        editor = monaco.editor.create(document.getElementById('editordiv'), {
+    // self.module = undefined;
+    amdRequire(["vs/editor/editor.main"], () => {
+        editor = monaco.editor.create(document.getElementById("editordiv"), {
             value: "{\n\t\"Hello\":\"world\"\n}",
             language: "yaml",
-
             lineNumbers: "on",
             roundedSelection: false,
             scrollBeyondLastLine: false,
             readOnly: false,
             theme: "vs-dark"
         });
-        
-        //Testing
-        // var yamlpath = path.join(__dirname, 'example.yaml');
-        // console.log(yamlpath);
-        // var yaml_text = ""
-        // fs.readFile(yamlpath, 'utf8', (err, res) => {
-        // if (!err) {
-        //     editor.setModel(monaco.editor.createModel(res, 'yaml'));
-        //     yaml_text = res;
-        // }
-        // });
 
-        var yaml_text = getYAMLText();
+        const yaml_text = getYAMLText();
         if(null != yaml_text){
-            editor.setModel(monaco.editor.createModel(yaml_text, 'yaml'));
+            editor.setModel(monaco.editor.createModel(yaml_text, "yaml"));
         }
 
-        //Content Change event
-        editor.onDidChangeModelContent(function (e) {
-            console.log('content changed');
+        // Content Change event
+        editor.onDidChangeModelContent((e) => {
+            console.log("content changed");
         });
     });
 })();
