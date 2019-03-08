591b62589a83788f6d5ea3b0fc0c0b6864665544
Priyan Vaithilingam <priyanmuthu@gmail.com>
2019-03-07 23:10:40 -0800

    missed a file

---
+++ src/renderUtils.js
@@ -0,0 +1,84 @@
+const utils = require('./utils.js');
+
+function createEditButton() {
+    var editButton = document.createElement('button');
+    editButton.classList.add('btn');
+    editButton.classList.add('btn-default');
+    editButton.classList.add('pull-right');
+    // editButton.innerText = "Edit";
+    editButton.style.minWidth = "40px";
+    editButton.style.marginRight = "10px";
+    var eIcon = document.createElement('i');
+    eIcon.classList.add('glyphicon');
+    eIcon.classList.add('glyphicon-pencil');
+    editButton.appendChild(eIcon);
+    // editButton.insertAdjacentHTML('beforeend', '<span class="glyphicon glyphicon-pencil" />');
+    return editButton;
+}
+
+function createRunButton() {
+    var runButton = document.createElement('button');
+    runButton.classList.add('btn');
+    runButton.classList.add('btn-primary');
+    runButton.classList.add('pull-right');
+    // runButton.innerText = "Run";
+    runButton.style.minWidth = "40px";
+    var rIcon = document.createElement('i');
+    rIcon.classList.add('glyphicon');
+    rIcon.classList.add('glyphicon-play');
+    runButton.appendChild(rIcon);
+    return runButton;
+}
+
+function createInfo(infoText) {
+    var infoIcon = document.createElement('span');
+    infoIcon.classList.add('glyphicon');
+    infoIcon.classList.add('glyphicon-info-sign');
+    infoIcon.setAttribute('data-toggle', 'tooltip');
+    infoIcon.setAttribute('title', infoText);
+    infoIcon.style.marginRight = "10px";
+    return infoIcon;
+}
+
+function createModal() {
+    var modalID = utils.getUniqueID();
+    var modalDiv = document.createElement('div');
+    modalDiv.id = modalID;
+    modalDiv.classList.add('modal');
+    modalDiv.classList.add('fade');
+    modalDiv.setAttribute('role', 'dialog');
+    modalDiv.style.maxHeight = constants.modalMaxHeight;
+    var modalDialogDiv = document.createElement('div');
+    modalDialogDiv.classList.add('modal-dialog');
+    modalDialogDiv.classList.add('modal-lg');
+    modalDialogDiv.classList.add('modal-dialog-centered');
+    modalDialogDiv.setAttribute('role', 'document');
+    modalDiv.appendChild(modalDialogDiv);
+    var modalContentDiv = document.createElement('div');
+    modalContentDiv.classList.add('modal-content');
+    modalDialogDiv.appendChild(modalContentDiv);
+    modalContentDiv.insertAdjacentHTML('beforeend',
+        `
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+    </div>
+    `);
+
+    var modalBodyDiv = document.createElement('div');
+    modalBodyDiv.style.maxHeight = constants.modalMaxHeight;
+    modalContentDiv.appendChild(modalBodyDiv);
+    modalBodyDiv.innerHTML = '';
+
+    var modalFooterDiv = document.createElement('div');
+    modalFooterDiv.classList.add('modal-footer');
+    modalContentDiv.appendChild(modalFooterDiv);
+
+    return { modalDiv: modalDiv, modalBodyDiv: modalBodyDiv, modalFooterDiv: modalFooterDiv, modalID: modalID };
+}
+
+module.exports = {
+    createEditButton: createEditButton,
+    createRunButton: createRunButton,
+    createModal: createModal,
+    createInfo: createInfo
+};
